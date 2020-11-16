e50a4dcb473683f093d0753ca161d66ee23d8db8
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-11-15 23:05:34 -0300

    feat: selecao_com_substituição

--- project/geracao_particoes.h
+++ project/geracao_particoes.h
@@ -13,7 +13,7 @@ void classificacao_interna(char *nome_arquivo_entrada, Nomes *nome_arquivos_said
 // nome_arquivo_entrada: nome do arquivo de entrada
 // nome_arquivo_saida: lista encadeada com nomes de arquivos de saida
 // M: tamanho do array em memoria para manipulacao dos registros
-_Noreturn void selecao_com_substituicao(char *nome_arquivo_entrada, Nomes *nome_arquivos_saida, int M);
+void selecao_com_substituicao(char *nome_arquivo_entrada, Nomes *nome_arquivos_saida, int M);
 
 // Executa o algoritmo de geracao de particoes por Selecao Natural
 // nome_arquivo_entrada: nome do arquivo de entrada
